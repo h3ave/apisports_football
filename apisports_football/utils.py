2dd039bbb6c4304338377efe80b8d36d7e6159df
h3ave <h3ave@github.com>
2024-10-23 09:31:07 +0300

    prepare_params util changed for boolean values

--- apisports_football/utils.py
+++ apisports_football/utils.py
@@ -3,4 +3,11 @@ from typing import Dict
 def prepare_params(params: Dict) -> Dict:
     if params is None:
         return {}
-    return {k: v for k, v in params.items() if v is not None}
+    result = {}
+    for k, v in params.items():
+        if v is not None:
+            if isinstance(v, bool):
+                result[k] = str(v).lower()
+            else:
+                result[k] = v
+    return result
