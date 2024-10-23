fd17b6453a739ad3508ef8b9b5d532e61926a76b
h3ave <h3ave@github.com>
2024-10-23 09:30:27 +0300

    added check api_key on string

--- apisports_football/apiclient.py
+++ apisports_football/apiclient.py
@@ -1,6 +1,8 @@
 import aiohttp
+
+from typing import Dict
 from .utils import prepare_params
-from .exceptions import ApiError
+from .exceptions import ApiError, ApiKeyTypeError
 
 
 class ApiClient:
@@ -9,6 +11,8 @@ class ApiClient:
 
     def __init__(self, api_key: str):
         self.api_key = api_key
+        if not isinstance(self.api_key, str):
+            raise ApiKeyTypeError('API key must be a string value')
         self.BASE_HEADERS['x-rapidapi-key'] = api_key
 
     async def __aenter__(self):
@@ -17,7 +21,7 @@ class ApiClient:
     async def __aexit__(self, *args, **kwargs):
         return None
 
-    async def _make_request(self, endpoint: str, params: dict = None, **kwargs) -> dict:
+    async def _make_request(self, endpoint: str, params: Dict = None, **kwargs) -> Dict:
         async with aiohttp.ClientSession(headers=self.BASE_HEADERS) as session:
             params = prepare_params(params=params)
             response = await session.request('GET', f'{self.BASE_URL}/{endpoint}', params=params, **kwargs)
