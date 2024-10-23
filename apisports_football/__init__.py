f610c6455bc60e5783ad67f57519bdb0f928bee2
h3ave <h3ave@github.com>
2024-10-23 09:29:52 +0300

    added import new exception

--- apisports_football/__init__.py
+++ apisports_football/__init__.py
@@ -1,7 +1,7 @@
 from apisports_football.wrapper import Wrapper
 
 from .apiclient import ApiClient
-from .exceptions import ApiError
+from .exceptions import ApiError, ApiKeyTypeError
 from .Models import *
 from .utils import prepare_params
 from .wrapper import Wrapper
