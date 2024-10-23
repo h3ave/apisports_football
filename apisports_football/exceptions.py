ddc30f3d977ca8dd8ad24c7b74775ad4c0849bd8
h3ave <h3ave@github.com>
2024-10-23 09:30:48 +0300

    created new exception ApiKeyTypeError

--- apisports_football/exceptions.py
+++ apisports_football/exceptions.py
@@ -1,3 +1,7 @@
 class ApiError(Exception):
     def __init__(self, message: str) -> None:
         super().__init__(message)
+
+class ApiKeyTypeError(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
